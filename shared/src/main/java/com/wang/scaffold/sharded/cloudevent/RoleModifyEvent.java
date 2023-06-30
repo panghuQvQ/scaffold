5f7f511839a7e1569066a6b7520378b0a7758d6a
panghuQvQ <1097155803@QQ.com>
2023-06-30 14:37:27 +0800

    update

--- shared/src/main/java/com/wang/scaffold/sharded/cloudevent/RoleModifyEvent.java
+++ shared/src/main/java/com/wang/scaffold/sharded/cloudevent/RoleModifyEvent.java
@@ -3,11 +3,17 @@ package com.wang.scaffold.sharded.cloudevent;
 import org.springframework.cloud.bus.event.RemoteApplicationEvent;
 
 /**
- * 自定义事件：
+ * 自定义事件对象：
  * Spring Cloud Bus的事件都继承于RemoteApplicationEvent类
  */
 public class RoleModifyEvent extends RemoteApplicationEvent {
 
+	/**
+	 * 显示生成 serialVersionUID，作用：
+	 * 在进行反序列化时，JVM会把传来的字节流中的serialVersionUID与本地相应实体（类）的serialVersionUID进行比较，
+	 * 如果相同就认为是一致的，可以进行反序列化，否则就会出现序列化版本不一致的异常。(InvalidClassException)
+	 *
+	 */
 	private static final long serialVersionUID = 1L;
 
 	public RoleModifyEvent() {

2add41e9a612ca908392f734eb0a58db7bd6539d
panghuQvQ <1097155803@QQ.com>
2023-10-08 16:58:07 +0800

    File Upload

--- core/src/main/java/com/wang/scaffold/consts/RedisKeys.java
+++ core/src/main/java/com/wang/scaffold/consts/RedisKeys.java
@@ -9,4 +9,7 @@ public class RedisKeys {
     /** 档案流水号 */
     public static final String FILE_SERIALNUMBER_PRE = "serialNumber";
 
+    /** 敏感信息访问 */
+    public static final String SENSITIVE_INFO_ACCESS_PREFIX = "sensitive-info-access:";
+
 }
